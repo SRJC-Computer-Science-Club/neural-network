3b72096eb6c60b4c1d7462150676131c66780adc
joshuasrjc <joshuasrjc@gmail.com>
2016-06-10 14:27:10 -0700

    Created Eclipse Workspace

---
+++ NeuralNetwork/src/club/srjccs/neuralnet/NeuralNetwork.java
@@ -0,0 +1,14 @@
+package club.srjccs.neuralnet;
+
+public class NeuralNetwork
+{
+	public static void main(String[] agrs)
+	{
+		new NeuralNetwork();
+	}
+	
+	NeuralNetwork()
+	{
+		System.out.println("Hello World");
+	}
+}
